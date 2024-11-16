eeecde91c4749ae24fe1a33c95de84ac9d9ac4fa
EthanTillmanAD <ethantillman101@gmail.com>
2024-11-16 00:42:37 -0500

    testing for phone screen fit

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function time(){
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
 
+    hours = hours ? hours :12;
     document.getElementById("clock").innerText = `${hours}:${minutes}:${seconds}  ${ampm}`;
 
 }
